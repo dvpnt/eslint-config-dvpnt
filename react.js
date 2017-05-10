e24341ec01742d7fa56e2f0f6573674b3e7ef295
fleg <flegmatik.bel@gmail.com>
2017-05-10 17:15:27 +0300

    add react config

---
+++ react.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+	'plugins': ['react-dvpnt'],
+	'extends': [
+		'eslint-config-airbnb/rules/react',
+		'eslint-config-airbnb/rules/react-a11y'
+	],
+	'env': {
+		'es6': true,
+		'browser': true
+	},
+	'rules': {
+		'react/jsx-indent': ['error', 'tab'],
+		'react/jsx-indent-props': ['error', 'tab'],
+		'react/forbid-prop-types': ['error', {'forbid': ['object', 'array']}],
+
+		'react/no-array-index-key': 'off',
+		'react/no-danger': 'off',
+		'react/prefer-es6-class': 'off',
+
+		'react-dvpnt/jsx-require-buttons-type': ['error', 'always']
+	}
+};
